d73fb3d69a23644c936eb933ed134581259e0739
Tristyn Turner <tristyngturner@gmail.com>
2021-04-22 20:09:27 -0500

    basic content

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render () {
+        return (
+        <div>
+            <h1 className="is-size-4 has-text-weight-bold has-text-centered has-text-light">Contact Us</h1>
+            <p className="is-size-6 has-text-light has-text-centered">Phone: (555) 555-5555</p>
+            <h3>Coded by: <a href="https://github.com/pannelltyler91" target="_blank">Tyler</a>,  
+            <a href="https://github.com/kaysto" target="_blank">Kayci</a> & 
+            <a href="https://github.com/tristyngturner" target="_blank">Tristyn</a> </h3>
+            
+        </div>
+        )
+    }
+}
+
+export default Footer;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Welcome to "What the, HTML?"</h1>
+                <p>Test your beginner coding knowledge with either a HTML, CSS or Javascript quiz.</p>
+            </div>
+        )
+    }
+}
+
+export default Home;
