eb558cee01d9f34e1400bf28512a5b567c3d5e10
Tristyn Turner <tristyngturner@gmail.com>
2021-04-27 19:50:55 -0500

    messed with quizzes and added backend stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,20 @@ import Home from '../components/Home';
 
 class Header extends Component {
 
-
+_getQuiz = () => {
+    document.getElementById('quiz').addEventListener('click', function(e) {
+        e.preventDefault();
+        let urlValue = e.target.innerHTML
+        console.log(urlValue)
+        fetch (`http://localhost:3001/api/quiz`+urlValue)
+        .then(response => response.json())
+        .then(data => console.log(data))
+    })
+}
 
     render () {
         return (
             <div>
-                <p>Header component</p>
                 <div className="top-page">
                     <nav className="navbar navbar-inverse navbar-fixed-top">
                         <div className="container">
@@ -22,7 +30,7 @@ class Header extends Component {
                             <div className="single-page-nav sticky-wrapper" id="tmNavbar">
                                 <ul className="nav navbar-nav">
                                     <li><a href="#section1">Home</a></li>
-                                    <li><a href="/Quiz">HTML</a></li>
+                                    <li><a id="quiz" href="/Quiz/HTML" onClick={this._getQuiz}>HTML</a></li>
                                     <li><a href="#section3">CSS</a></li>
                                     <li><a href="#section4">Javascript</a></li>
                                 </ul>
@@ -30,10 +38,30 @@ class Header extends Component {
                         </div>
                     </nav>
                 </div>
+
+                <form>
+                    <div id="question">Question</div>
+                
+                    <input type="radio" name="answer" value="answer1"></input>
+                    <label htmlFor="answer1" id="answer1" >Answer 1</label>
+                    <input type="radio" name="answer" value="answer2"></input>
+                    <label htmlFor="answer2" id="answer2">Answer 2</label><br/>
+                    <button id="nextButton">Next</button>
+                    
+                
+                </form>
                 <Switch>
                     <Route exact path='/' component={Home}></Route>
-                    <Route path="/Quiz" component={Quiz}></Route>
-        </Switch>
+                    {/* <Route path="/Quiz" component={Quiz}></Route> */}
+                    <Route path="/Quiz/HTML">
+                        <Quiz quizType="HTML"/>
+                        
+                    </Route>
+                    <Route path="/Quiz/CSS">
+                        <Quiz quizType="CSS"/>
+                        
+                    </Route>
+                </Switch>
             </div>
         )
     }
