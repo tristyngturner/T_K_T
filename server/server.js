114a0dc37a60d66698aa9d7c788e5e72a7ff2e74
Tyler Pannell <pannelltyler91@gmail.com>
2021-05-04 21:14:26 -0400

    changed port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001 ;
 const db = require('./models')
 const{qData} = require('./permdata');
 const cors = require('cors')
